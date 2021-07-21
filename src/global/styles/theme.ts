4a4a83341d8e233e6d26e5dbddb125c93371acc9
Flávio Ferreira <business.flavioferreira@gmail.com>
2021-07-21 14:55:28 -0300

    Start configs

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,20 @@
+export default {
+  colors: {
+    primary: '#111423',
+    secondary: '#1B1E2F',
+
+    success: '#12A454',
+    attention: '#E83F5B',
+
+    titles: '#FFFFFF',
+    texts: '#969CB2',
+ 
+    purple: '#685CF3',
+  },
+
+  fonts: {
+    regular: 'Poppins_400Regular',
+    medium: 'Poppins_500Medium',
+    bold: 'Poppins_700Bold'
+  }
+}
---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+import { Text } from 'react-native';
+
+export function SignIn(){
+  return (
+    <Container>
+      <Text>Texte</Text>
+    </Container>
+  );
+}
